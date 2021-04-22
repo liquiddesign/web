497db947729b4740e7a2e4c19ebd42cebe8f228e
Oldrich <oldrich@lqd.cz>
2021-04-22 14:23:50 +0200

    tiny templetes

--- src/DB/TinyTemplateRepository.php
+++ src/DB/TinyTemplateRepository.php
@@ -8,5 +8,18 @@ use StORM\Repository;
 
 class TinyTemplateRepository extends Repository
 {
-
+	public function getTemplates()
+	{
+		$suffix = $this->getConnection()->getMutationSuffix();
+		
+		$collection  = $this->many()->setSelect([
+			'title' => "name$suffix",
+			'description' => "description$suffix",
+			'content' => "html",
+		]);
+		
+		$collection->orderBy(['priority'])->setIndex(null)->setFetchClass(\StdClass::class);
+		
+		return $collection->toArray();
+	}
 }
