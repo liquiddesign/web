530fd9d9146792605124dc96877fcba1055ae4f0
honza952 <honza@lqd.cz>
2021-07-14 21:31:06 +0200

    widget template

--- src/Controls/Widget.php
+++ src/Controls/Widget.php
@@ -12,27 +12,32 @@ use Nette;
 final class Widget extends Nette\Application\UI\Component
 {
 	private ITextboxFactory $textboxFactory;
-	
+
 	private IGalleryFactory $galleryFactory;
-	
+
 	private ITestimonialFactory $testimonialFactory;
-	
+
 	private IContactFactory $contactFactory;
-	
+
 	private ICarouselFactory $carouselFactory;
-	
+
 	private IFaqFactory $faqFactory;
-	
+
 	private ITabFactory $tabFactory;
-	
+
 	private IVideoFactory $videoFactory;
-	
+
 	private IBannerFactory $bannerFactory;
-	
+
 	private IMapFactory $mapFactory;
-	
+
 	private IHubspotFactory $hubspotFactory;
-	
+
+	/**
+	 * @var string[]|null[]
+	 */
+	private array $defaultTemplates = [];
+
 	public function __construct(
 		ITextboxFactory $textboxFactory,
 		IGalleryFactory $galleryFactory,
@@ -58,77 +63,87 @@ final class Widget extends Nette\Application\UI\Component
 		$this->mapFactory = $mapFactory;
 		$this->hubspotFactory = $hubspotFactory;
 	}
-	
+
+	public function setDefaultTemplate(string $class, ?string $template): void
+	{
+		$this->defaultTemplates[$class] = $template;
+	}
+
 	public function createComponentTextbox()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->textboxFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentGallery()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->galleryFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentTestimonial()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->testimonialFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentContact()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->contactFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentCarousel()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->carouselFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentFaq()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
-			return $this->faqFactory->create($id);
+			$faqControl = $this->faqFactory->create($id);
+			$faqControl->onAnchor[] = function (Faq $faqControl): void {
+				$faqControl->template->setFile($this->defaultTemplates[Faq::class] ?? null);
+			};
+			
+			return $faqControl;
 		});
 	}
-	
+
 	public function createComponentTab()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->tabFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentVideo()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->videoFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentBanner()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->bannerFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentMap()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
 			return $this->mapFactory->create($id);
 		});
 	}
-	
+
 	public function createComponentHubspot()
 	{
 		return new Nette\Application\UI\Multiplier(function ($id) {
