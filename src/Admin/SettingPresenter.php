b77f8ebf720bdc77b7c996b5a72817584230ae00
Petr Křehlík <petr@lqd.cz>
2021-11-02 13:39:49 +0100

    headCode

--- src/Admin/SettingPresenter.php
+++ src/Admin/SettingPresenter.php
@@ -7,87 +7,103 @@ namespace Web\Admin;
 use Admin\BackendPresenter;
 use Admin\Controls\AdminForm;
 use Admin\Controls\AdminGrid;
+use Nette\Forms\Form;
 use Web\DB\ContactItem;
 use Web\DB\ContactItemRepository;
 use Web\DB\SettingRepository;
-use Nette\Forms\Form;
 
 class SettingPresenter extends BackendPresenter
 {
 	protected const CONFIGURATION = [
-		'groups' => ['company', 'support', 'map'],
+		'groups' => [
+			'company',
+			'support',
+			'map'
+		],
+		'allowedSettings' => [
+			'headCode'
+		]
 	];
-	
+
 	/** @inject */
 	public SettingRepository $settingsRepo;
-	
+
 	/** @inject */
 	public ContactItemRepository $contactItemRepo;
-	
+
 	public function beforeRender(): void
 	{
 		parent::beforeRender();
-		
+
 		$this->template->tabs = [
 			'@default' => 'Hlavní informace a mapa',
 			'@contacts' => 'Kontakty',
 			'@social' => 'Sítě',
+			'@others' => 'Ostatní',
 		];
 	}
-	
+
 	public function actionDefault()
 	{
-		/** @var AdminForm $form */
+		/** @var \Admin\Controls\AdminForm $form */
 		$form = $this->getComponent('form');
-		
+
 		$form->setDefaults($this->settingsRepo->many()->setIndex('name')->toArrayOf('value'));
 	}
-	
+
 	public function actionSocial()
 	{
-		/** @var AdminForm $form */
+		/** @var \Admin\Controls\AdminForm $form */
 		$form = $this->getComponent('socialForm');
-		
+
 		$form->setDefaults($this->settingsRepo->many()->setIndex('name')->toArrayOf('value'));
 	}
-	
+
+	public function actionOthers()
+	{
+		/** @var \Admin\Controls\AdminForm $form */
+		$form = $this->getComponent('othersForm');
+
+		$form->setDefaults($this->settingsRepo->many()->setIndex('name')->toArrayOf('value'));
+	}
+
 	public function renderDefault()
 	{
-		$this->template->headerLabel = 'Kontakty a sítě';
+		$this->template->headerLabel = 'Kontakty';
 		$this->template->headerTree = [
-			['Kontakty a sítě'],
+			['Kontakty'],
 		];
 		$this->template->displayButtons = [];
 		$this->template->displayControls = [$this->getComponent('form')];
 	}
-	
+
 	public function renderSocial()
 	{
-		$this->template->headerLabel = 'Kontakty a sítě';
+		$this->template->headerLabel = 'Sítě';
 		$this->template->headerTree = [
-			['Kontakty a sítě'],
+			['Sítě'],
 		];
 		$this->template->displayButtons = [];
 		$this->template->displayControls = [$this->getComponent('socialForm')];
 	}
-	
+
 	public function createComponentForm(): AdminForm
 	{
 		$form = $this->formFactory->create();
-		
+
 		if (\in_array('company', static::CONFIGURATION['groups'])) {
 			$form->addGroup('Společnost');
 			$form->addText('companyName', 'Název společnosti')->setNullable();
 			$form->addTextArea('legalInfo', 'Informace o zápisu')->setHtmlAttribute('cols', 70)->setNullable();
 		}
-		
+
 		if (\in_array('support', static::CONFIGURATION['groups'])) {
 			$form->addGroup('Podpora');
 			$form->addText('supportEmail', 'E-mail')->setNullable()->addCondition(Form::FILLED)->addRule($form::EMAIL);
 			$form->addText('supportPhone', 'Telefon')->setNullable();
 			$form->addText('supportPhoneTime', 'Dostupnost telefonu')->setNullable()->setHtmlAttribute('data-info', 'Zvolte libovolný formát');
 		}
-		
+
 		if (\in_array('map', static::CONFIGURATION['groups'])) {
 			$form->addGroup('Mapa');
 			$form->addText('contactStreet', 'Ulice')->setHtmlAttribute('data-info', 'Např.: Josefská 15')->setNullable();
@@ -95,68 +111,68 @@ class SettingPresenter extends BackendPresenter
 			$form->addText('contactGPSx', 'GPS souřadnice X')->setHtmlAttribute('data-info', 'GPS souřadnice pro zobrazení bodu na mapě. Např.: 16.6125203')->setNullable();
 			$form->addText('contactGPSy', 'GPS souřadnice Y')->setHtmlAttribute('data-info', 'GPS souřadnice pro zobrazení bodu na mapě. Např.: 49.1920700')->setNullable();
 		}
-		
+
 		$form->addSubmit('submit', 'Uložit');
-		
+
 		$form->onSuccess[] = function (AdminForm $form) {
 			$values = $form->getValues('array');
-			
+
 			foreach ($values as $key => $value) {
 				$this->settingsRepo->syncOne(['name' => $key, 'value' => $value]);
 			}
-			
+
 			$this->flashMessage('Nastavení uloženo', 'success');
 			$form->processRedirect('default');
 		};
-		
+
 		return $form;
 	}
-	
+
 	public function createComponentSocialForm(): AdminForm
 	{
 		$form = $this->formFactory->create();
-		
+
 		$form->addGroup('Sítě');
-		
+
 		$form->addText('socialFacebook', 'Facebook')->setNullable();
 		$form->addText('socialInstagram', 'Instagram')->setNullable();
 		$form->addText('socialTwitter', 'Twitter')->setNullable();
-		
+
 		$form->addSubmit('submit', 'Uložit');
-		
+
 		$form->onSuccess[] = function (AdminForm $form) {
 			$values = $form->getValues('array');
-			
+
 			foreach ($values as $key => $value) {
 				$this->settingsRepo->syncOne(['name' => $key, 'value' => $value]);
 			}
-			
+
 			$this->flashMessage('Nastavení uloženo', 'success');
 			$form->processRedirect('social');
 		};
-		
+
 		return $form;
 	}
-	
+
 	public function renderContacts(): void
 	{
 		$this->template->headerLabel = 'Kontakty a sítě';
 		$this->template->headerTree = [
 			['Kontakty a sítě', 'default']
 		];
-		
-		
+
+
 		$this->template->displayButtons = [$this->createNewItemButton('newContact')];
 		$this->template->displayControls = [$this->getComponent('contactsGrid')];
 	}
-	
+
 	public function actionDetailContact(ContactItem $contactItem): void
 	{
 		/** @var AdminForm $form */
 		$form = $this->getComponent('contactForm');
 		$form->setDefaults($contactItem->toArray());
 	}
-	
+
 	public function renderNewContact(): void
 	{
 		$this->template->headerLabel = 'Kontakty a sítě';
@@ -164,12 +180,12 @@ class SettingPresenter extends BackendPresenter
 			['Kontakty a sítě', 'default'],
 			['Nový kontakt']
 		];
-		
+
 		$this->template->displayButtons = [$this->createBackButton('contacts')];
 		$this->template->displayControls = [$this->getComponent('contactForm')];
 		$this->template->activeTab = 'contacts';
 	}
-	
+
 	public function renderDetailContact(ContactItem $contactItem): void
 	{
 		$this->template->headerLabel = 'Nastavení webu - Kontakty';
@@ -180,39 +196,39 @@ class SettingPresenter extends BackendPresenter
 		];
 		$this->template->displayButtons = [$this->createBackButton('contacts')];
 		$this->template->displayControls = [$this->getComponent('contactForm')];
-		
+
 		$this->template->activeTab = 'contacts';
 	}
-	
+
 	public function createComponentContactForm(): AdminForm
 	{
 		$form = $this->formFactory->create(true);
-		
+
 		$form->addLocaleText('name', 'Název');
 		$form->addText('phone', 'Telefonní čísla')->setHtmlAttribute('data-info', 'Zadejte telefonní čísla oddělená středníkem ";"');
 		$form->addText('email', 'E-maily')->setHtmlAttribute('data-info', 'Zadejte e-maily oddělené středníkem ";"');
 		$form->addInteger('priority', 'Priorita')->setDefaultValue(10)->setRequired();
 		$form->addCheckbox('hidden', 'Skryto');
-		
+
 		$form->addSubmits(!$this->getParameter('contactItem'));
-		
-		$form->onSuccess[] = function (AdminForm $form) {
+
+		$form->onSuccess[] = function (AdminForm $form): void {
 			$values = $form->getValues('array');
-			
+
 			$contactItem = $this->contactItemRepo->syncOne($values, null, true);
-			
+
 			$this->flashMessage('Uloženo', 'success');
 			$form->processRedirect('detailContact', 'contacts', [$contactItem]);
 		};
-		
+
 		return $form;
 	}
-	
+
 	public function createComponentContactsGrid(): AdminGrid
 	{
 		$grid = $this->gridFactory->create($this->contactItemRepo->many(), 20, 'priority');
 		$grid->addColumnSelector();
-		
+
 		$grid->addColumnText('Název', 'name', '%s', 'name');
 		$grid->addColumnText('Telefon', 'phone', '<a href="tel:%1$s"><i class="fa fa-phone-alt"></i> %1$s</a>')->onRenderCell[] = [$grid, 'decoratorEmpty'];
 		$grid->addColumnText('E-mail', 'email', '<a href="mailto:%1$s"><i class="far fa-envelope"></i> %1$s</a>')->onRenderCell[] = [$grid, 'decoratorEmpty'];
@@ -222,13 +238,48 @@ class SettingPresenter extends BackendPresenter
 		$grid->addColumnMutations('name', false);
 		$grid->addColumnLinkDetail('detailContact');
 		$grid->addColumnActionDelete();
-		
+
 		$grid->addButtonSaveAll();
 		$grid->addButtonDeleteSelected();
-		
+
 		$grid->addFilterTextInput('search', ['name_cs', 'phone', 'email'], null, 'Název, telefon, e-mail');
 		$grid->addFilterButtons(['contacts']);
-		
+
 		return $grid;
+	}
+
+	public function renderOthers(): void
+	{
+		$this->template->headerLabel = 'Ostatní nastavení';
+		$this->template->headerTree = [
+			['Ostatní nastavení', 'default']
+		];
+
+		$this->template->displayButtons = [];
+		$this->template->displayControls = [$this->getComponent('othersForm')];
+	}
+
+	public function createComponentOthersForm(): AdminForm
+	{
+		$form = $this->formFactory->create(true);
+
+		if (isset($this::CONFIGURATION['allowedSettings']) && \in_array('headCode', $this::CONFIGURATION['allowedSettings'])) {
+			$form->addTextArea('headCode', 'HTML kód hlavičky')->setNullable()->setHtmlAttribute('data-info', 'Tento kód bude vložen jako poslední prvek hlavičky.');
+		}
+
+		$form->addSubmit('submit', 'Uložit');
+
+		$form->onSuccess[] = function (AdminForm $form): void {
+			$values = $form->getValues('array');
+
+			foreach ($values as $key => $value) {
+				$this->settingsRepo->syncOne(['name' => $key, 'value' => $value]);
+			}
+
+			$this->flashMessage('Uloženo', 'success');
+			$this->redirect('this');
+		};
+
+		return $form;
 	}
 }
