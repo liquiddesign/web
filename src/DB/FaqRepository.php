38db513c19b26b06f9c2ee7274e22f2cfecad903
Petr Křehlík <petr@lqd.cz>
2022-03-02 17:32:02 +0100

    faq - collection method

--- src/DB/FaqRepository.php
+++ src/DB/FaqRepository.php
@@ -4,8 +4,23 @@ declare(strict_types=1);
 
 namespace Web\DB;
 
+use StORM\Collection;
 use StORM\Repository;
 
+/**
+ * @extends \StORM\Repository<\Web\DB\Faq>
+ */
 class FaqRepository extends Repository
 {
+	public function getCollection(bool $includeHidden = false): Collection
+	{
+		$suffix = $this->getConnection()->getMutationSuffix();
+		$collection = $this->many();
+
+		if (!$includeHidden) {
+			$collection->where("this.active$suffix", true);
+		}
+
+		return $collection->orderBy(["this.name$suffix",]);
+	}
 }
